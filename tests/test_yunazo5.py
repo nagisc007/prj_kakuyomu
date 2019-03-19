538e9eca61822f3f170cc77fbf61694b859aac98
N.T.WORKS <nagisc007@yahoo.co.jp>
2019-03-19 21:20:48 +0900

    Implement #49: yunazo5 outline (#52)
    * [WIP] wait for word class [ci skip]
    
    * Updated: submodule - word class [ci skip]
    
    * [WIP] for update submodule [ci skip]
    
    * Updated: submodule - believe [ci skip]
    
    * Fixed: story outline
    
    Resolved #49

--- tests/test_yunazo5.py
+++ tests/test_yunazo5.py
@@ -4,34 +4,49 @@
 import unittest
 
 from storybuilder.builder.acttypes import ActType, Behavior
-from storybuilder.builder.testtools import is_all_actions
-from storybuilder.builder.testtools import has_basic_infos
-from storybuilder.builder.testtools import has_outline_infos
+import storybuilder.builder.testtools as testtools
 
 from yunazo.story5.story import story
 from yunazo.story5.story import intro
 from yunazo.story5.story import strange_oldman
 from yunazo.story5.story import real_criminal
 
+from yunazo.story5.story import Yusha, Panna, Crades, Claire
+from yunazo.story5.story import Donston
+
 
 class StoryTest(unittest.TestCase):
     """Test story.
     * outline
-        - Who:
-        - Whom:
-        - When:
-        - Where:
-        - What:
-        - Why:
-        - How:
-        - Result:
+        - Who: 勇者
+        - Whom: クラデス（容疑者）
+        - When: 六月末（クラデスと出会う前）
+        - Where: ドンストンの警察本部
+        - What: クラデスの容疑を晴らす
+        - Why: クラデスはやっていない
+        - How: 真犯人を暴く
+        - Result: 店を継ぎたくない息子の犯行だった
     """
     @classmethod
     def setUpClass(cls):
         print("**** story of yunazo5 ****")
 
     def setUp(self):
-        pass
+        self.story = story()
+
+    def test_is_all_actions(self):
+        self.assertTrue(testtools.is_all_actions(self, self.story))
+
+    def test_has_basic_infos(self):
+        self.assertTrue(testtools.has_basic_infos(self, self.story, Yusha(), Crades()))
+
+    def test_has_outline_infos(self):
+        self.assertTrue(testtools.has_outline_infos(self, self.story,
+            Yusha(), Behavior.DISPEL, "容疑",
+            Yusha(), Behavior.BELIEVE, "やっていない",
+            Yusha(), Behavior.TELL, "真犯人を見つける",
+            Claire(), Behavior.CONFESS, "継ぎたくない")
+            )
 
 
 class EpisodeTest(unittest.TestCase):
--- yunazo/story5/story.py
+++ yunazo/story5/story.py
@@ -9,10 +9,61 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append('common')
 
 from storybuilder.builder.acttypes import Behavior
-from storybuilder.builder.base import Master
+from storybuilder.builder.base import Stage, Item, DayTime, Word, Master
+from storybuilder.builder.person import Person
 from storybuilder.builder.tools import build_to_story
 
 from common import Yusha, Panna, Crades
+
+
+# characters
+class Claire(Person):
+    def __init__(self):
+        super().__init__("クレア", 20, "female", "靴屋の娘", "あたし", "事件の真犯人")
+
+
+class PoliceChief(Person):
+    def __init__(self):
+        super().__init__("警官", 36, "male", "警官", note="町の警官")
+
+
+class LePoir(Person):
+    def __init__(self):
+        super().__init__("ル・ポワ", 55, "male", "探偵", "私", "グレート島で名を馳せる名探偵")
+
+
+# stages
+class Donston(Stage):
+    def __init__(self):
+        super().__init__("ドンストン", "グレート島中北部の町。競馬で有名。")
+
+# items
+class MagicBall(Stage):
+    def __init__(self):
+        super().__init__("魔法玉", "異次元に飛ばす魔力を詰めた玉")
+
+# words
+class MagiNet(Word):
+    def __init__(self):
+        super().__init__("魔導ネット", "魔術により")
+
+
+# days
+class Today(DayTime):
+    def __init__(self):
+        super().__init__("逮捕日", 6, 10, 1018, 15, "クラデスと出会った日")
+
+class FirstDay(DayTime):
+    def __init__(self):
+        super().__init__("第一の事件日", 6, 1, 1018)
+
+class SecondDay(DayTime):
+    def __init__(self):
+        super().__init__("第二の事件日", 6, 5, 1018)
+
+class ThirdDay(DayTime):
+    def __init__(self):
+        super().__init__("第三の事件日", 6, 9, 1018)
 
 
 def intro(ma: Master):
@@ -34,11 +85,46 @@ def story():
     '''Create a story object.
     '''
     ma = Master("Yunazo5")
+    yusha, panna, crades = Yusha(), Panna(), Crades()
+    claire, poli, lepoir = Claire(), PoliceChief(), LePoir()
+    town = Donston()
+    ball = MagicBall()
+    net = MagiNet()
+    day = Today()
+
     return ma.story(
             ma.title("勇者は冒険よりＡＢＣ商店消失の謎解きをしたい"),
             intro(ma),
             strange_oldman(ma),
-            real_criminal(ma)
+            real_criminal(ma),
+            day.explain("六月の湿気が多い午後"),
+            town.explain("競馬で有名な町"),
+            yusha.come(town.name),
+            panna.come(town.name),
+            town.explain("中央広場で騒ぎ"),
+            yusha.go("広場"),
+            poli.catch(crades.name),
+            crades.tell("何もしとらん"),
+            poli.explain("{}が事件の重要参考人".format(crades.name)),
+            lepoir.talk("事件について"),
+            claire.tell("突然店が消えたんです"),
+            lepoir.talk("数日前から連続して商店が消えたのだ"),
+            yusha.dispel("{}の容疑".format(crades.name)),
+            yusha.believe("{}はやっていない".format(crades.name)),
+            # 信じてる根拠
+            yusha.talk("犯人を見つける"),
+            lepoir.laugh(""),
+            yusha.hear("それぞれの事情"),
+            crades.talk("ずっと飲んだくれていた"),
+            claire.talk("最近は何でも魔導ネットを使うから商売にならない"),
+            yusha.tell("真犯人を見つける"),
+            yusha.search("消失場所"),
+            yusha.go("店があった場所"),
+            town.explain("地面から綺麗に区切られたように消えている"),
+            panna.tell("まるで魔法みたい"),
+            yusha.ask("どんな店だったか"),
+            claire.talk("汚い店で、大嫌いだったと文句"),
+            claire.confess("店を継ぎたくない"),
             )
 
 def main():
