3ddbce8219092d34dd0f928d3e5d4ae80b5dbf71
N.T.WORKS <nagisc007@yahoo.co.jp>
2019-03-23 20:32:32 +0900

    Added: episode outline (#72)
    resolved #66

--- tests/test_yunazo7.py
+++ tests/test_yunazo7.py
@@ -12,6 +12,8 @@ from yunazo.story7.story import story
 from yunazo.story7.story import ep_intro, ep_in_dream, ep_out_dream
 from yunazo.story7.story import Yusha, Panna, Crades, Emile
 from yunazo.story7.story import noardo
+from yunazo.story7.story import dream_syndrome, wall_memo
+from yunazo.story7.story import visit_day
 
 
 class StoryTest(unittest.TestCase):
@@ -34,7 +36,7 @@ class StoryTest(unittest.TestCase):
         vila = noardo()
         self.assertTrue(testtools.has_outline_infos(self, self.story,
             Yusha().search("",obj=vila),
-            Yusha().know("",obj=vila),
+            Yusha().know("村の噂",obj=vila),
             Yusha().visit("",obj=vila),
             Yusha().wake(),
             ))
@@ -47,9 +49,14 @@ class EpisodeTest(unittest.TestCase):
 
     def setUp(self):
         self.ma = Master('test')
-        self.ep1 = ep_intro(self.ma)
-        self.ep2 = ep_in_dream(self.ma)
-        self.ep3 = ep_out_dream(self.ma)
+        self.ep1 = ep_intro(self.ma, Yusha(), Panna(), Crades(), Emile(),
+                noardo(), visit_day())
+        self.ep2 = ep_in_dream(self.ma, Yusha(), Emile(), Panna(), Crades(),
+                dream_syndrome(), wall_memo(),
+                noardo(), visit_day())
+        self.ep3 = ep_out_dream(self.ma, Yusha(), Emile(), Panna(), Crades(),
+                dream_syndrome(),
+                noardo(), visit_day())
 
     def test_ep1_is_all_actions(self):
         self.assertTrue(testtools.is_all_actions(self, self.ep1))
@@ -61,19 +68,40 @@ class EpisodeTest(unittest.TestCase):
         self.assertTrue(testtools.is_all_actions(self, self.ep3))
 
     def test_ep1_has_basic_infos(self):
-        pass
+        self.assertTrue(testtools.has_basic_infos(self, self.ep1,
+            Yusha(), Emile()))
 
     def test_ep2_has_basic_infos(self):
-        pass
+        self.assertTrue(testtools.has_basic_infos(self, self.ep2,
+            Yusha(), Emile()))
 
     def test_ep3_has_basic_infos(self):
-        pass
+        self.assertTrue(testtools.has_basic_infos(self, self.ep3,
+            Emile(), Yusha()))
 
     def test_ep1_has_outline_infos(self):
-        pass
+        vila = noardo()
+        self.assertTrue(testtools.has_outline_infos(self, self.ep1,
+            Yusha().investigate("", vila),
+            Yusha().know("村の噂", vila),
+            Yusha().visit("", vila),
+            Panna().sleep(),
+            ))
 
     def test_ep2_has_outline_infos(self):
-        pass
+        dreamsyn = dream_syndrome()
+        memo = wall_memo()
+        self.assertTrue(testtools.has_outline_infos(self, self.ep2,
+            Yusha().solve("", dreamsyn),
+            Panna().sleep(),
+            Yusha().read("", memo),
+            Yusha().sleep(),
+            ))
 
     def test_ep3_has_outline_infos(self):
-        pass
+        self.assertTrue(testtools.has_outline_infos(self, self.ep3,
+            Emile().wake("", Yusha()),
+            Yusha().sleep(),
+            Emile().hear("解除方法", Crades()),
+            Emile().kiss("", Yusha())
+            ))
--- yunazo/story7/story.py
+++ yunazo/story7/story.py
@@ -22,6 +22,9 @@ def noardo():
     return Stage("ノアルド", "全員が眠る村")
 
 # items
+def wall_memo():
+    return Item("壁のメモ", "壁にびっしり書かれた謎の文字")
+
 
 # words
 def dream_syndrome():
@@ -33,16 +36,64 @@ def visit_day():
 
 
 # episodes
-def ep_intro(ma: Master):
+def ep_intro(ma: Master,
+        yusha: Yusha, panna: Panna, crades: Crades, emile: Emile,
+        vila: Stage,
+        day: DayTime):
     return ma.story(
+            vila.explain("そこは全員が眠る村だった"),
+            day.explain("小春日和の午後"),
+            yusha.visit("", obj=vila),
+            yusha.investigate("", vila),
+            yusha.know("村の噂",obj=vila),
+            yusha.search("",obj=vila),
+            panna.visit("",obj=vila),
+            crades.visit("",obj=vila),
+            emile.visit("",obj=vila),
+            vila.explain("全員本当に眠っている"),
+            panna.sleep(),
+            crades.sleep(),
             )
 
-def ep_in_dream(ma: Master):
+def ep_in_dream(ma: Master,
+        yusha: Yusha, emile: Emile, panna: Panna, crades: Crades,
+        dreamsyn: Word, memo: Item,
+        vila: Stage,
+        day: DayTime):
     return ma.story(
+            panna.sleep(),
+            crades.sleep(),
+            yusha.leave("二人", emile),
+            yusha.solve("", dreamsyn),
+            vila.explain("村の教会を訪れる"),
+            memo.explain("地下室の壁にびっしり書かれている"),
+            yusha.read("", memo),
+            day.explain("いつの間にか夕闇に"),
+            emile.ask("夢中になるもの", yusha),
+            yusha.reply("謎解き", emile),
+            yusha.talk("謎解き"),
+            yusha.dream("幼い頃のこと"),
+            yusha.sleep(),
             )
 
-def ep_out_dream(ma: Master):
+def ep_out_dream(ma: Master,
+        yusha: Yusha, emile: Emile, panna: Panna, crades: Crades,
+        dreamsyn: Word,
+        vila: Stage,
+        day: DayTime):
     return ma.story(
+            day.explain("まだ村に来て一時間と経っていない"),
+            day.explain("外は明るい"),
+            vila.explain("宿屋のベッドの上に運んだ"),
+            panna.talk("眠ったまま",obj=yusha),
+            yusha.sleep(),
+            emile.wake("", yusha),
+            emile.hear("解除方法", crades),
+            crades.teach("{}の解除方法".format(dreamsyn.name)),
+            dreamsyn.explain("その人物に夢中な人からのキスにより解除"),
+            panna.kiss("",obj=yusha),
+            emile.kiss("",obj=yusha),
+            yusha.wake(),
             )
 
 
@@ -51,37 +102,20 @@ def story():
     ma = Master("yunazo7")
     yusha, panna, crades, emile = Yusha(), Panna(), Crades(), Emile()
     vila = noardo()
+    memo = wall_memo()
     dreamsyn = dream_syndrome()
     vday = visit_day()
 
     return ma.story(
             ma.title("勇者は冒険より夢中病の謎解きをしたい"),
-            ep_intro(ma),
-            ep_in_dream(ma),
-            ep_out_dream(ma),
-            vila.explain("そこは全員が眠る村だった"),
-            vday.explain("小春日和の午後"),
-            yusha.visit("", obj=vila),
-            yusha.know("",obj=vila),
-            yusha.search("",obj=vila),
-            panna.visit("",obj=vila),
-            crades.visit("",obj=vila),
-            emile.visit("",obj=vila),
-            vila.explain("全員本当に眠っている"),
-            panna.sleep(),
-            crades.sleep(),
-            yusha.leave("二人", emile),
-            emile.ask("夢中になるもの", yusha),
-            yusha.reply("謎解き", emile),
-            yusha.talk("謎解き"),
-            yusha.dream("幼い頃のこと"),
-            yusha.sleep(),
-            panna.talk("眠ったまま",obj=yusha),
-            crades.teach("{}の解除方法".format(dreamsyn.name)),
-            dreamsyn.explain("その人物に夢中な人からのキスにより解除"),
-            panna.kiss("",obj=yusha),
-            emile.kiss("",obj=yusha),
-            yusha.wake(),
+            ep_intro(ma, yusha, panna, crades, emile,
+                vila, vday),
+            ep_in_dream(ma, yusha, emile, panna, crades,
+                dreamsyn, memo,
+                vila, vday),
+            ep_out_dream(ma, yusha, emile, panna, crades,
+                dreamsyn,
+                vila, vday),
             )
 
 
