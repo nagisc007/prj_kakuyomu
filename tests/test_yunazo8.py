20dd446b45d75b8f1ce7fca9393c5cb3a324696b
N.T.WORKS <nagisc007@yahoo.co.jp>
2019-03-26 10:27:43 +0900

    implement #80: episode outlines (#85)
    * Added: episode basic info
    
    * Added: episode outlines
    
    resolved #80

--- tests/test_yunazo8.py
+++ tests/test_yunazo8.py
@@ -9,9 +9,9 @@ import storybuilder.builder.testtools as testtools
 from yunazo.story8.story import story
 from yunazo.story8.story import ep_intro, ep_maou, ep_festa
 from yunazo.story8.story import Yusha, Panna, Crades, Emile
-from yunazo.story8.story import boy_ordy
-from yunazo.story8.story import zibonga_vila
-from yunazo.story8.story import devils_fes
+from yunazo.story8.story import boy_ordy, mam_ordy, dad_ordy, vila_people
+from yunazo.story8.story import zibonga_vila, ordy_home
+from yunazo.story8.story import devils_fes, real_tale
 from yunazo.story8.story import past_day, before_day, fes_day
 
 class StoryTest(unittest.TestCase):
@@ -44,13 +44,48 @@ class StoryTest(unittest.TestCase):
 class EpisodesTest(unittest.TestCase):
 
     def setUp(self):
-        pass
+        ma = Master('test yunazo8')
+        self.ep1 = ep_intro(ma, Yusha(), Panna(), Crades(), Emile(), boy_ordy(),
+                devils_fes(), zibonga_vila(), before_day())
+        self.ep2 = ep_maou(ma, Yusha(), Panna(), Crades(), Emile(), boy_ordy(),
+                mam_ordy(), dad_ordy(),
+                devils_fes(), zibonga_vila(), ordy_home(), before_day(), past_day())
+        self.ep3 = ep_festa(ma, Yusha(), Panna(), Crades(), Emile(), boy_ordy(),
+                vila_people(),
+                devils_fes(), real_tale(), zibonga_vila(), fes_day())
 
     def test_episodes_has_basic_infos(self):
-        # for p1, p2, p3 in tables:
-        # with self.subTest(param1=p1,param2=p2...)
-        # self.assertEqual()
-        pass
+        data_set = [
+                ("ep1", self.ep1, Yusha(), boy_ordy()),
+                ("ep2", self.ep2, Yusha(), boy_ordy()),
+                ("ep3", self.ep3, Yusha(), boy_ordy()),
+                ]
+        for title, ep, hero, rival in data_set:
+            with self.subTest(title=title, ep=ep, hero=hero, rival=rival):
+                self.assertTrue(testtools.has_basic_infos(self, ep, hero, rival))
 
     def test_episodes_has_outline_infos(self):
-        pass
+        data_set = [
+                ("ep1", self.ep1,
+                    Yusha().investigate(devils_fes()),
+                    Yusha().hear(devils_fes()),
+                    Yusha().visit(zibonga_vila()),
+                    Yusha().meet(boy_ordy()),
+                    ),
+                ("ep2", self.ep2,
+                    Yusha().hear(boy_ordy(), "祭り"),
+                    boy_ordy().know(devils_fes()),
+                    Yusha().teach(boy_ordy(), "勇者"),
+                    boy_ordy().teach(past_day()),
+                    ),
+                ("ep3", self.ep3,
+                    boy_ordy().rescue(Yusha()),
+                    Yusha().catch(vila_people()).ps(),
+                    boy_ordy().talk(real_tale()),
+                    Yusha().rescue(boy_ordy()).ps(),
+                    ),
+                ]
+        for title, ep, what, why, how, result in data_set:
+            with self.subTest(title=title, ep=ep, what=what, why=why, how=how, result=result):
+                self.assertTrue(testtools.has_outline_infos(self, ep,
+                    what, why, how, result))
--- yunazo/story8/story.py
+++ yunazo/story8/story.py
@@ -19,10 +19,22 @@ from common import Yusha, Panna, Crades, Emile
 def boy_ordy():
     return Person("オルディ", 12, "male", "少年", "おれ", "勇者に憧れる少年")
 
+def mam_ordy():
+    return Person("オルディ母", 30, "female", "機織り", "わたし")
+
+def dad_ordy():
+    return Person("オルディ父", 28, "male", "漁師", "俺", "三年前に亡くなる")
+
+def vila_people():
+    return Person("村人", 30, "male", "村の人間")
+
 
 # stages
 def zibonga_vila():
     return Stage("ジボンガ", "魔王を崇めている集落")
+
+def ordy_home():
+    return Stage("オルディの家", "板と布を重ねた粗末な家")
 
 
 # items
@@ -32,10 +44,12 @@ def zibonga_vila():
 def devils_fes():
     return Word("魔王祭", "魔王を崇める祭。今年で三周年")
 
+def real_tale():
+    return Word("真実の話", "三年前に魔王が助けたという話の真実")
 
 # daytimes
 def past_day():
-    return DayTime("魔王の日", 4, 1, 1016, note="魔王により村が救われた日。三年前")
+    return DayTime("三年前", 4, 1, 1016, note="魔王により村が救われた日。三年前")
 
 def before_day():
     return DayTime("祭り前日", 3, 31, 1019)
@@ -45,38 +59,82 @@ def fes_day():
 
 
 # episodes
-def ep_intro(ma: Master):
+def ep_intro(ma: Master,
+        yusha: Yusha, panna: Panna, crades: Crades, emile: Emile, ordy: Person,
+        fes: Word,
+        vila: Stage,
+        day: DayTime):
     return ma.story("魔王を崇める村",
+            day.explain("祭りの前日"),
+            vila.explain("行われている", fes),
+            yusha.visit(vila, "仲間と共に"),
+            panna.visit(vila),
+            yusha.hear(fes),
+            yusha.want(fes, "知る"),
+            yusha.investigate(fes),
+            yusha.investigate(vila),
+            yusha.meet(ordy),
+            ordy.glad(yusha, "出会えて"),
+            ordy.invite(yusha, "自分の家"),
             )
 
-def ep_maou(ma: Master):
+def ep_maou(ma: Master,
+        yusha: Yusha, panna: Panna, crades: Crades, emile: Emile,
+        ordy: Person, mam: Person, dad: Person,
+        fes: Word,
+        vila: Stage, home: Stage,
+        day: DayTime, pday: DayTime):
     return ma.story("魔王と勇者",
+            day.explain("夕闇が落ちてきた"),
+            home.explain("風雨をなんとかしのげる程度の粗末の木造家屋"),
+            yusha.think(ordy, "貧しい"),
+            ordy.talk(yusha, "勇者"),
+            yusha.hear(ordy, "祭り"),
+            ordy.know(fes),
+            ordy.want(yusha),
+            yusha.teach(ordy, "勇者"),
+            ordy.teach(pday),
+            yusha.know(dad, "三年前に亡くなる"),
             )
 
-def ep_festa(ma: Master):
+def ep_festa(ma: Master,
+        yusha: Yusha, panna: Panna, crades: Crades, emile: Emile, ordy: Person,
+        people: Person,
+        fes: Word, tale: Word,
+        vila: Stage,
+        day: DayTime):
     return ma.story("魔王の謎",
+            yusha.know(vila, info="魔王が救った"),
+            day.explain("祭りの朝だった"),
+            yusha.wake(),
+            vila.explain("中央の広場で"),
+            yusha.bind(),
+            yusha.catch(people).ps(),
+            ordy.rescue(yusha),
+            ordy.tell("魔王はそんなこと望んでない"),
+            ordy.talk(tale),
+            ordy.teach(info="魔王はかつて勇者だった"),
+            yusha.rescue(ordy).ps(),
             )
 
 # story
 def story():
     ma = Master('yunazo 08')
     yusha, panna, crades, emile = Yusha(), Panna(), Crades(), Emile()
-    ordy = boy_ordy()
-    vila = zibonga_vila()
-    fes = devils_fes()
+    ordy, mam, dad = boy_ordy(), mam_ordy(), dad_ordy()
+    people = vila_people()
+    vila, home = zibonga_vila(), ordy_home()
+    fes, tale = devils_fes(), real_tale()
     pday, bday, fday = past_day(), before_day(), fes_day()
     return ma.story("勇者は冒険より三周年魔王祭の謎解きをしたい",
-            ep_intro(ma),
-            ep_maou(ma),
-            ep_festa(ma),
-            bday.explain("祭りの前日"),
-            vila.explain("行われている", fes),
-            yusha.visit(vila, "仲間と共に"),
-            panna.visit(vila),
-            yusha.hear(fes),
-            yusha.want(fes, "知る"),
-            yusha.investigate(vila),
-            yusha.know(vila, info="魔王が救った"),
+            ep_intro(ma, yusha, panna, crades, emile, ordy,
+                fes, vila, bday),
+            ep_maou(ma, yusha, panna, crades, emile, ordy,
+                mam, dad,
+                fes, vila, home, bday, pday),
+            ep_festa(ma, yusha, panna, crades, emile, ordy,
+                people,
+                fes, tale, vila, fday),
             )
 
 # main
